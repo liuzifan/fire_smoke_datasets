fe9f89e82281e44bacdaee6990219622931c094d
liuzifan <462850643@qq.com>
2021-11-30 15:48:04 +0800

    rm

--- labels/scrips.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-pic_names = os.listdir('./train/')
-# label_names = os.listdir('./labels/')
-count = 0
-for i, name in enumerate(pic_names):
-    i = i+818
-    os.rename('./train/'+name, './train/fire_smoke_'+str(i)+'.txt')
-    # os.rename('./labels/'+label_names[i], './labels/fire_smoke_'+str(i)+'.txt')
